55ba32af7870232f54d5241c1b3693de004e8d76
Robert Jensen <jensen.robert@gmail.com>
2022-08-15 09:50:01 +0200

    Added gated 4w measurement

--- cryostat-raspi01/cryostat_measurement.py
+++ cryostat-raspi01/cryostat_measurement.py
@@ -1,5 +1,6 @@
 import time
-import logging
+# import logging
+import threading
 
 import Gpib  # linux-gpib, user space wrapper to kernel driver
 
@@ -17,13 +18,17 @@ import credentials
 
 CURRENT_MEASUREMENT_PROTOTYPE = {
     'type': None,  # DC-sweep, AC-sweep
+    'error': None,
     'start_time': 0,
     'current_time': 0,
     'current': [],
     'v_total': [],
     'v_sample': [],
-    'v_gate': []
+    'theta': [],
+    'v_backgate': [],  # Back gate voltage
+    'i_backgate': []  # Bakc gate leak-current
 }
+
 
 class CrystatMeasurement(object):
     def __init__(self):
@@ -35,11 +40,13 @@ class CrystatMeasurement(object):
         self.back_gate = Keithley2400(interface='gpib', gpib_address=22)
 
         self.chamber_name = "dummy"
+
+        self.lock_in_frequency = None
+        self.set_lock_in_frequency(1000)
         self.data_set_saver = DataSetSaver("measurements_" + self.chamber_name,
                                            "xy_values_" + self.chamber_name,
                                            credentials.user, credentials.passwd)
         self.data_set_saver.start()
-        
 
     def _identify_all_instruments(self):
         found_all = True
@@ -50,7 +57,7 @@ class CrystatMeasurement(object):
         else:
             print('Did NOT find 2181A at GPIB 7!!!!!!')
             found_all = False
-        
+
         lock_in_id = self.lock_in.read_software_version()
         if lock_in_id.find('SR830') > 0:
             # print('Found SRS 830 at GPIB 8. Lock-In amplifier')
@@ -66,7 +73,7 @@ class CrystatMeasurement(object):
         else:
             print('Did NOT find 6221 at GPIB 12!!!!!!')
             found_all = False
-            
+
         dmm_id = self.dmm.read_software_version()
         if dmm_id.find('MODEL 2000') > 0:
             # print('Found Keithley 2000 at GPIB 16. General purpose DMM')
@@ -100,14 +107,20 @@ class CrystatMeasurement(object):
         if not found_all:
             self._scan_gpib()
 
-    def reset_current_measurement(self, measurement_type):
+    def set_lock_in_frequency(self, frequency: float):
+        self.lock_in_frequency = frequency
+        # TODO: Configure SR830 with suitable time-constants
+        return frequency
+
+    def reset_current_measurement(self, measurement_type, error=False):
         """
         Reset current data if a new measurement is about to start.
         If measurement_type is None, this indicates that the measurement
         stops, in this case keep the data for now.
         """
-        if measurement_type is None:         
+        if measurement_type is None:
             self.current_measurement['type'] = None
+            self.current_measurement['type'] = error
         else:
             self.current_measurement = CURRENT_MEASUREMENT_PROTOTYPE.copy()
             self.current_measurement.update(
@@ -131,7 +144,7 @@ class CrystatMeasurement(object):
                 self.current_measurement[key].append(value)
                 self.data_set_saver.save_point(key, (now, value))
         self.current_measurement['current_time'] = time.time()
-        
+
     def _add_metadata(self, labels, meas_type, comment, timestep=None, freq=None):
         metadata = {
             'Time': CustomColumn(time.time(), "FROM_UNIXTIME(%s)"),
@@ -154,7 +167,7 @@ class CrystatMeasurement(object):
         source_ok = self.current_source.read_status()
         if not source_ok:
             print('ABORT')
-            self.reset_current_measurement(None)
+            self.reset_current_measurement(None, error=True)
             self.current_source.stop_and_unarm()
             self.current_source.set_current(0)
             self.current_source.output_state(False)
@@ -170,7 +183,7 @@ class CrystatMeasurement(object):
             step_list.append(current)
         # yield?
         return step_list
-    
+
     def dc_4_point_measurement(self, start: float, stop: float, steps: int):
         """
         Perform a 4-point DC iv-measurement.
@@ -194,7 +207,6 @@ class CrystatMeasurement(object):
 
         for current in self._calculate_steps(start, stop, steps):
             self.current_source.set_current(current)
-            print(current)
             time.sleep(0.02)
             if not self._check_I_source_status():
                 return
@@ -206,32 +218,20 @@ class CrystatMeasurement(object):
         self.current_source.output_state(False)
         self.reset_current_measurement(None)
 
-    def ac_4_point_measurement(self, start: float, stop: float, steps: int):
+    def _ac_4_point_sweep(self, steps):
         """
-        Perform a 2-point DC iv-measurement.
-        :param start: The lowest current in the sweep.
-        :param stop: The highest current in the sweep.
-        :steps: Number of steps in the sweep.
+        Perform an actual ac-sweep. Typically this will happen as a part
+        of a larger sweep of some other parameter, could be gate voltage,
+        magnetic field or temperature.
         """
-        FREQ = 1976
-        labels = {'v_total': 'Vtotal', 'v_sample': 'Vsample',
-                  'current': 'Current', 'theta': 'Theta'}
-        self._add_metadata(labels, 202, 'AC measurement', timestep=0.02, freq=FREQ)
-            
-        self.reset_current_measurement('ac_sweep')
-        # # todo: If software-timed sweep is not good enough, look into sweeping 6221
-
-        # # Configure instruments:
-        self.current_source.stop_and_unarm()
-        self.current_source.set_voltage_limit(1)
-        self.current_source.set_current_range(stop)
-        self.current_source.source_sine_wave(FREQ, start)
-        
-        for current in self._calculate_steps(start, stop, steps):
+        # todo: If software-timed sweep is not good enough, look into sweeping 6221
+        time.sleep(1)
+        for current in steps:
             # The current source use peak height as unit, everything
             # else use rmd, multiply by sqrt(2) to unify
             self.current_source.set_wave_amplitude(current * 1.4142)
-            time.sleep(0.02)
+            # Wait for ~10 cycles to ensure everything is settled
+            time.sleep(10.0 / self.lock_in_frequency)
             if not self._check_I_source_status():
                 return
 
@@ -242,15 +242,126 @@ class CrystatMeasurement(object):
                     'v_sample': v_sample, 'theta': theta}
             self.add_to_current_measurement(data)
 
+    def _init_ac(self, start, stop):
+        """
+        Setup the current source for an AC measurement.
+        :param start: This is the start value of the sweep and will be
+        used a default amplitude until the actual measurement start.
+        :param stop: The final value of the sweep, will be used to
+        set the range.
+        """
+        self.current_source.stop_and_unarm()
+        self.current_source.set_voltage_limit(1)
+        self.current_source.set_current_range(stop)
+        self.current_source.source_sine_wave(self.lock_in_frequency, start)
+        return True
+
+    def ac_4_point_measurement(self, start: float, stop: float, total_steps: int):
+        """
+        Perform a 4-point AC iv-measurement.
+        :param start: The lowest current in the sweep.
+        :param stop: The highest current in the sweep.
+        :steps: Number of steps in the sweep.
+        """
+        labels = {'v_total': 'Vtotal', 'v_sample': 'Vsample',
+                  'current': 'Current', 'theta': 'Theta'}
+        self._add_metadata(labels, 202, 'AC measurement',
+                           freq=self.lock_in_frequency)
+
+        self.reset_current_measurement('ac_sweep')
+        self._init_ac(start, stop)  # Configure current source
+        current_steps = self._calculate_steps(start, stop, total_steps)
+        t = threading.Thread(target=self._ac_4_point_sweep,
+                             args=(current_steps,))
+        t.start()
+        while t.is_alive():
+            print('waiting')
+            time.sleep(1)
+        # TODO!! Handle the case of non-succes!
+        # if self.current_measurement['error']:
+
         # Indicate that the measurment is completed
         self.current_source.stop_and_unarm()
         self.reset_current_measurement(None)
 
-        
+    def _read_gate(self):
+        voltage = self.back_gate.read_voltage()
+        current = self.back_gate.read_current()
+        data = {
+            'v_backgate': voltage,
+            'i_backgate': current
+        }
+        self.add_to_current_measurement(data)
+        return current
+
+    def gated_ac_4_point_measurement(
+            self,
+            i_start: float, i_stop: float, total_i_steps: int,
+            backgate_from, backgate_to, gate_steps
+    ):
+        """
+        Perform a gated 4-point AC iv-measurement.
+        :param i_start: The lowest current in the sweep.
+        :param i_stop: The highest current in the sweep.
+        :param total_i_steps: Number of steps in the sweep.
+        :param backgate_from:
+        :param backgate_to:
+        :param gate_steps:
+        """
+        labels = {
+            'v_total': 'Vtotal', 'v_sample': 'Vsample',
+            'current': 'Current', 'theta': 'Theta',
+            'v_backgate': 'Back Gate Voltage', 'i_backgate': 'Back Gate Leak'
+        }
+        self._add_metadata(labels, 203, 'Gated AC measurement',
+                           freq=self.lock_in_frequency)
+
+        self.reset_current_measurement('gated_ac_sweep')
+
+        # Todo: Set correct ranges for compliance and source
+        self.back_gate.set_source_function('v')
+        self.back_gate.set_current_limit(1e-4)
+        self.back_gate.set_voltage(backgate_from)
+        self.back_gate.output_state(True)
+        time.sleep(0.1)
+        self._read_gate()
+
+        gate_steps = self._calculate_steps(backgate_from, backgate_to, gate_steps)
+
+        self._init_ac(i_start, i_stop)  # Configure current source
+        current_steps = self._calculate_steps(i_start, i_stop, total_i_steps)
+
+        for gate_v in gate_steps:
+            self.back_gate.set_voltage(gate_v)
+            time.sleep(0.5)
+            self._read_gate()
+
+            t = threading.Thread(target=self._ac_4_point_sweep,
+                                 args=(current_steps,))
+            t.start()
+            while t.is_alive():
+                # Backgate can be measured slower than the inner sweep,
+                # we do it periodically while waiting for the sweep
+                print('waiting')
+                time.sleep(1)
+                self._read_gate()
+            self._read_gate()
+
+        # TODO!! Handle the case of non-succes!
+        # if self.current_measurement['error']:
+
+        # Indicate that the measurment is completed
+        self.current_source.stop_and_unarm()
+        self.back_gate.set_voltage(0)
+        self.back_gate.output_state(False)
+        self.reset_current_measurement(None)
+
     def test(self):
         self.instrument_id()
         # self.dc_4_point_measurement(1e-6, 1e-4, 100)
-        self.ac_4_point_measurement(1e-6, 1e-4, 100)
+        # self.ac_4_point_measurement(1e-6, 1e-4, 100)
+        # self.ac_4_point_measurement(1e-7, 1e-5, 100)
+        self.gated_ac_4_point_measurement(1e-7, 1e-5, 100, 0, 10, 10)
 
 
 if __name__ == '__main__':
