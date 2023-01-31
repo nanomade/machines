2650445d12adcfbaa5e92acbf8a749494e9bc413
Robert Jensen <jensen.robert@gmail.com>
2023-01-31 10:23:28 +0100

    Settings for Edwards Roughing pump logger

---
+++ moorfield-nxds-raspi01/settings.py
@@ -0,0 +1,5 @@
+# setup = 'dateplots_nXDS_pumps'
+table = 'dateplots_nXDS_pumps'
+channels = {'usb-FTDI_USB-RS232_Cable_AU04GUGQ-if00-port0': 'moorfield_hv_etching_rp',
+            'usb-FTDI_USB-RS232_Cable_AU04Q44R-if00-port0': 'moorfield_deposition_rp'}
+
