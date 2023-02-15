9030e0d91173e287675a8b32d234a872b9964f44
Robert Jensen <jensen.robert@gmail.com>
2023-02-15 09:04:52 +0100

    Minor refactoring + a few new helper functions.

--- linkam-raspi01/linkam_measurement_base.py
+++ linkam-raspi01/linkam_measurement_base.py
@@ -1,6 +1,5 @@
 import time
 # import logging
-import threading
 
 import Gpib  # linux-gpib, user space wrapper to kernel driver
 
@@ -115,14 +114,57 @@ class LinkamMeasurementBase(object):
             reply = device.read()
             print('{}: {}'.format(i, reply))
 
+    def _read_gate(self):
+        voltage = self.back_gate.read_voltage()
+        current = self.back_gate.read_current()
+        data = {'v_backgate': voltage, 'i_backgate': current}
+        self.add_to_current_measurement(data)
+        return current
+
+    def _read_current_sources(self):
+        current_1 = self.source_logger_1.read_ac_voltage() / 1e6
+        current_2 = self.source_logger_2.read_ac_voltage() / 1e6
+        data = {'current_1': current_1, 'current_2': current_2}
+        self.add_to_current_measurement(data)
+        return current_1, current_2
+
+    def _prepare_gate(self, compliance):
+        self.back_gate.set_source_function('v')
+        self.back_gate.set_current_limit(compliance)
+        self.back_gate.set_voltage(0)
+        self.back_gate.output_state(True)
+        time.sleep(1)
+
+    def _add_metadata(self, labels, meas_type, comment, timestep=None, freq=None):
+        try:
+            # Test if sql-connection is still alive
+            self.data_set_saver.get_unique_values_from_measurements('type')
+        except self.data_set_saver.MySQLdb._exceptions.OperationalError:
+            self._restart_data_set_saver()
+
+        timestamp = CustomColumn(time.time(), "FROM_UNIXTIME(%s)")
+        for key, value in labels.items():
+            metadata = {
+                'Time': timestamp,
+                'label': None,
+                'type': meas_type,
+                'comment': comment,
+                'timestep': timestep,
+                'frequency': freq
+            }
+
+            if isinstance(value, str):
+                metadata.update({'label': value})
+            else:  # value is dict
+                metadata.update(value)
+
+            self.data_set_saver.add_measurement(key, metadata)
+        return True
+
     def instrument_id(self):
         found_all = self._identify_all_instruments()
         if not found_all:
             self._scan_gpib()
-
-    def set_lock_in_frequency(self, frequency: float):
-        # Will be re-implemented in AC classes
-        pass
 
     def reset_current_measurement(self, measurement_type, error=False):
         """
@@ -159,116 +201,9 @@ class LinkamMeasurementBase(object):
                 self.data_set_saver.save_point(key, (now, value))
         self.current_measurement['current_time'] = time.time()
 
-    def _add_metadata(self, labels, meas_type, comment, timestep=None, freq=None):
-        # Test that the sql-connection is still alive - if it fails, this would be
-        # a good chance to restart.
-        # Print is only for debuging, should be replaced with try-except
-        try:
-            # Test if sql-connection is still alive
-            self.data_set_saver.get_unique_values_from_measurements('type')
-        except self.data_set_saver.MySQLdb._exceptions.OperationalError:
-            self._restart_data_set_saver()
-
-        timestamp = CustomColumn(time.time(), "FROM_UNIXTIME(%s)")
-        for key, value in labels.items():
-            metadata = {
-                'Time': timestamp,
-                'label': None,
-                'type': meas_type,
-                'comment': comment,
-                'timestep': timestep,
-                'frequency': freq
-            }
-
-            if isinstance(value, str):
-                metadata.update({'label': value})
-            else:  # value is dict
-                metadata.update(value)
-
-            self.data_set_saver.add_measurement(key, metadata)
-        return True
-
     def abort_measurement(self):
         print('ABORT')
         self.reset_current_measurement(None, error='Aborted')
-
-    def _read_gate(self):
-        voltage = self.back_gate.read_voltage()
-        current = self.back_gate.read_current()
-        data = {
-            'v_backgate': voltage,
-            'i_backgate': current
-        }
-        self.add_to_current_measurement(data)
-        return current
-
-    def gated_ac_4_point_measurement(
-            self,
-            i_start: float, i_stop: float, total_i_steps: int,
-            backgate_from, backgate_to, gate_steps
-    ):
-        """
-        Perform a gated 4-point AC iv-measurement.
-        :param i_start: The lowest current in the sweep.
-        :param i_stop: The highest current in the sweep.
-        :param total_i_steps: Number of steps in the sweep.
-        :param backgate_from:
-        :param backgate_to:
-        :param gate_steps:
-        """
-        labels = {
-            'v_total': 'Vtotal', 'v_sample': 'Vsample',
-            'current': 'Current', 'theta': 'Theta',
-            'v_backgate': 'Back Gate Voltage', 'i_backgate': 'Back Gate Leak'
-        }
-        self._add_metadata(labels, 203, 'Gated AC measurement',
-                           freq=self.lock_in_frequency)
-
-        self.reset_current_measurement('gated_ac_sweep')
-
-        # Todo: Set correct ranges for compliance and source
-        self.back_gate.set_source_function('v')
-        self.back_gate.set_current_limit(1e-4)
-        self.back_gate.set_voltage(backgate_from)
-        self.back_gate.output_state(True)
-        time.sleep(0.1)
-        self._read_gate()
-
-        gate_steps = self._calculate_steps(backgate_from, backgate_to, gate_steps)
-
-        self._init_ac(i_start, i_stop)  # Configure current source
-        current_steps = self._calculate_steps(i_start, i_stop, total_i_steps)
-
-        for gate_v in gate_steps:
-            if self.current_measurement['type'] is None:
-                continue
-            self.back_gate.set_voltage(gate_v)
-            time.sleep(0.5)
-            self._read_gate()
-
-            t = threading.Thread(target=self._ac_4_point_sweep,
-                                 args=(current_steps,))
-            t.start()
-            while t.is_alive():
-                if self.current_measurement['type'] is None:
-                    # Measurement has been aborted
-                    t.stop()
-
-                # Backgate can be measured slower than the inner sweep,
-                # we do it periodically while waiting for the sweep
-                print('waiting')
-                time.sleep(1)
-                self._read_gate()
-            self._read_gate()
-
-        # TODO!! Handle the case of non-succes!
-        # if self.current_measurement['error']:
-
-        # Indicate that the measurment is completed
-        self.current_source.stop_and_unarm()
-        self.back_gate.set_voltage(0)
-        self.back_gate.output_state(False)
-        self.reset_current_measurement(None)
 
 
 if __name__ == '__main__':
