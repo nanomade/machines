9356827a0e7a66d1f6fe7057d201e2bbdbc0bc21
Robert Jensen <jensen.robert@gmail.com>
2023-08-28 12:02:40 +0200

    Small mechanism to allow running parrallel voltage measurements
    This might end up not being relevant though, if the trigger and
    READ:FRESh? works perfectly, they will return with no waiting
    - time will tell.

---
+++ cryostat-raspi01/electrical_measurements/cryostat_threaded_voltage.py
@@ -0,0 +1,54 @@
+import time
+
+
+class BackgroundMeasure():
+    def __init__(self):
+        self.voltage = None
+
+    def read_voltage(self):
+        while self.voltage is None:
+            time.sleep(0.01)
+        voltage = self.voltage
+        self.voltage = None
+        return voltage
+
+    def start_measurement(self):
+        raise NotImplementedError
+
+
+class MeasureVxx(BackgroundMeasure):
+    def __init__(self, current_source):
+        super().__init__()
+        self.current_source = current_source
+
+    def start_measurement(self):
+        self.current_source._2182a_comm(':DATA:FRESh?')
+        value_raw = self.current_source._2182a_comm()
+        try:
+            value_raw = value_raw.replace(chr(0x13), '')
+            voltage = float(value_raw)
+        except ValueError:
+            voltage = -1001
+        self.voltage = voltage
+
+
+class MeasureVxy(BackgroundMeasure):
+    def __init__(self, nano_v):
+        super().__init__()
+        self.nano_v = nano_v
+
+    def start_measurement(self):
+        voltage = self.nano_v.read_fresh()
+        if voltage is None:
+            voltage = -1001
+        self.voltage = voltage
+
+
+class MeasureVTotal(BackgroundMeasure):
+    def __init__(self, dmm):
+        super().__init__()
+        self.dmm = dmm
+
+    def start_measurement(self):
+        voltage = self.dmm.next_reading()
+        self.voltage = voltage
