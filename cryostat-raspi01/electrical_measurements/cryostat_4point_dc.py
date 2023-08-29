a565d9161f78f78649c7b5977ea07a5391178c2f
Robert Jensen <jensen.robert@gmail.com>
2023-08-29 15:26:00 +0200

    Functional version of 4-point DC - needs cleaning.

--- cryostat-raspi01/electrical_measurements/cryostat_4point_dc.py
+++ cryostat-raspi01/electrical_measurements/cryostat_4point_dc.py
@@ -1,45 +1,40 @@
 import time
+import threading
 
-from PyExpLabSys.drivers.keithley_2182 import Keithley2182
-from cryostat_measurement_base import CryostatMeasurementBase
+# from cryostat_measurement_base import CryostatMeasurementBase
+from cryostat_dc_base import CryostatDCBase
 
 
-class Cryostat4PointDC(CryostatMeasurementBase):
+class Cryostat4PointDC(CryostatDCBase):
     def __init__(self):
         super().__init__()
-
-        print(self.current_source.read_software_version())
-        # self.dmm and self.current_source are always in the base
-        # self.nanov1 = Keithley2182(interface='gpib', gpib_address=7)
 
     def abort_measurement(self):
         print('ABORT')
         self.reset_current_measurement(None, error='Aborted')
 
-    def dc_4_point_measurement(self, comment, start: float, stop: float, steps: int, **kwargs):
+    def dc_4_point_measurement(self, comment, start: float, stop: float, steps: int, v_limit=1.0, **kwargs):
         """
         Perform a 4-point DC iv-measurement.
         :param start: The lowest current in the sweep.
         :param stop: The highest current in the sweep.
         :steps: Number of steps in the sweep.
+        :v_limit: Maximal allowed voltage, default is 1.0
         """
-        # TODO!!! Take the comment as a parameter
-        labels = {'v_total': 'Vtotal', 'v_sample': 'Vsample', 'current': 'Current',
+        labels = {'v_total': 'Vtotal', 'v_xx': 'Vxx', 'v_xy': 'Vxy', 'current': 'Current',
                   'b_field': 'B-Field', 'vti_temp': 'VTI Temperature', 'sample_temp': 'Sample temperature'}
         self._add_metadata(labels, 201, comment, timestep=0.2)
         self.reset_current_measurement('dc_sweep')
-        # todo: If software-timed sweep is not good enough, look into sweeping 6221
 
         # Configure instruments:
-        self.current_source.set_voltage_limit(1)
-        self.current_source.set_current_range(stop)
-        self.current_source.set_current(0)
-        self.current_source.output_state(True)
-        # self.nanov1.set_range(0, 0)
-        # self.nanov1.set_integration_time(10)
+        self._configure_back_gate()  # Misused to trigger 2182a's
+        self._configure_dmm(v_limit)
+        self._configure_source(v_limit=v_limit, current_range=stop)
+        self._configure_nano_voltmeters(5)  # todo: Configurable nplc
 
-        self.current_source._2182a_comm(':CONF:VOLT')
-        self.current_source._2182a_comm(':SAMP:COUNT 1')
+        time.sleep(1)
+        self.current_source.set_current(start)
+
         for current in self._calculate_steps(start, stop, steps):
             if self.current_measurement['type'] is None:
                 # Measurement has been aborted, skip through the
@@ -47,36 +42,24 @@ class Cryostat4PointDC(CryostatMeasurementBase):
                 continue
 
             self.current_source.set_current(current)
-            time.sleep(0.02)
+            time.sleep(0.05)
             if not self._check_I_source_status():
                 return
 
-            print()
-            print('current', current)
-            value_raw = ''
-            while value_raw == '':
-                self.current_source._2182a_comm(':SENSE:CHANNEL 1')
-                self.current_source._2182a_comm(':READ?')
-                value_raw = self.current_source._2182a_comm()
-                print('sample_raw: ', value_raw)
-            try:
-                v_sample = float(value_raw)
-            except ValueError:
-                print('Unable to parse: {} as float'.format(repr(value_raw)))
-                continue
-            value_raw = ''
-            while value_raw == '':
-                self.current_source._2182a_comm(':SENSE:CHANNEL 2')
-                self.current_source._2182a_comm(':READ?')
-                value_raw = self.current_source._2182a_comm()
-                print('total_raw: ', value_raw)
-            try:
-                v_total = float(value_raw)
-            except ValueError:
-                print('Unable to parse: {} as float'.format(repr(value_raw)))
-                continue
+            data = self._read_voltages(5, store_gate=False)
+            # # t = threading.Thread(target=self._ac_4_point_sweep,
+            # #                      args=(current_steps,))
+            # t_vxx = threading.Thread(target=self.masure_voltage_xx.start_measurement)
+            # t_vxx.start()
+            # t_vtotal = threading.Thread(target=self.masure_voltage_total.start_measurement)
+            # t_vtotal.start()
 
-            data = {'current': current, 'v_total': v_total, 'v_sample': v_sample}
+            # t = time.time()
+            # v_xx = self.masure_voltage_xx.read_voltage()
+            # v_total = self.masure_voltage_total.read_voltage()
+            # print('Measure time: {}'.format(time.time() - t))
+            data['current'] = current
+            # data = {'current': current, 'v_total': v_total, 'v_xx': v_xx}
             self.add_to_current_measurement(data)
             self._read_cryostat()
 
