27154aff32511170b288ed42c9f34b41e90f5a6a
Robert Jensen <jensen.robert@gmail.com>
2023-10-05 09:25:31 +0200

    Small test-script for the socket server

---
+++ noise-spectrum-raspi01/test.py
@@ -0,0 +1,30 @@
+import time
+import json
+import socket
+
+SOCK = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+SOCK.setblocking(0)
+
+command = {
+        'cmd': 'abort',
+    }
+
+# command = {
+#     'cmd': 'start_measurement',
+#     'measurement': 'record_noise_spectrum',
+#     # 'measurement': 'record_xy_spectrum',
+#     # 'measurement': 'record_sweeped_xy_measurement',
+#     'comment': 'Test',
+#     'freq_high': 1e5,
+#     'freq_low': 100,
+#     'steps': 10,
+# }
+
+
+socket_command = 'json_wn#' + json.dumps(command)
+print(socket_command)
+SOCK.sendto(socket_command.encode(), ('127.0.0.1', 8500))
+time.sleep(0.01)
+recv = SOCK.recv(65535)
+print(recv)
+
