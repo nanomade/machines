634938207166d0a8bc8fd4ebfeda2e9e5d398117
Robert Jensen <jensen.robert@gmail.com>
2023-10-06 09:36:05 +0200

    Added some more cases to test script

--- noise-spectrum-raspi01/test.py
+++ noise-spectrum-raspi01/test.py
@@ -5,20 +5,21 @@ import socket
 SOCK = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 SOCK.setblocking(0)
 
-command = {
-        'cmd': 'abort',
-    }
-
 # command = {
-#     'cmd': 'start_measurement',
-#     'measurement': 'record_noise_spectrum',
-#     # 'measurement': 'record_xy_spectrum',
-#     # 'measurement': 'record_sweeped_xy_measurement',
-#     'comment': 'Test',
-#     'freq_high': 1e5,
-#     'freq_low': 100,
-#     'steps': 10,
-# }
+#         'cmd': 'abort',
+#     }
+
+command = {
+    'cmd': 'start_measurement',
+    # 'measurement': 'record_noise_spectrum',
+    'measurement': 'record_xy_spectrum',
+    # 'measurement': 'record_sweeped_xy_measurement',
+    'acquisition_time': 15,
+    'comment': 'Test',
+    'freq_high': 1e5,
+    'freq_low': 100,
+    'steps': 8,
+}
 
 
 socket_command = 'json_wn#' + json.dumps(command)
@@ -27,4 +28,12 @@ SOCK.sendto(socket_command.encode(), ('127.0.0.1', 8500))
 time.sleep(0.01)
 recv = SOCK.recv(65535)
 print(recv)
+
+
+# socket_command = 'measurement_running#json'
+# print(socket_command)
+# SOCK.sendto(socket_command.encode(), ('127.0.0.1', 9000))
+# time.sleep(0.01)
+# recv = SOCK.recv(65535)
+# print(recv)
 
