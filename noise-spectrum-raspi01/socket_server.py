43059c58f8884e3f2f7535d4816c1124b14f3c13
Robert Jensen <jensen.robert@gmail.com>
2023-10-06 09:27:06 +0200

    The socket server is now actually functional
    Measurements can be started and aborted.

--- noise-spectrum-raspi01/socket_server.py
+++ noise-spectrum-raspi01/socket_server.py
@@ -4,83 +4,79 @@ import time
 from record_spectrum import NoiseSpectrumRecorder
 from PyExpLabSys.common.sockets import DateDataPullSocket
 from PyExpLabSys.common.sockets import DataPushSocket
+
 # from PyExpLabSys.common.sockets import LiveSocket
 
 
 class NoiseSpectrumRecorderControl(threading.Thread):
-    """ Keep updated values of the current flow """
+    """Keep updated values of the current data"""
+
     def __init__(self):
         threading.Thread.__init__(self)
         self.nsr = NoiseSpectrumRecorder()
-        # socket_names = []
-        # timeouts = []
-        # for device in devices:
-        #     socket_names.append(device)
-        #     socket_names.append(device + '_setpoint')
-        #     timeouts.append(3.0)
-        #     timeouts.append(1e9)
 
-        # self.pullsocket = DateDataPullSocket(
-        #     name,
-        #     socket_names,
-        #     timeouts=timeouts,
-        #     port=9000
-        # )
-        # self.pullsocket.start()
-        # for device in devices:
-        #     addr = self.mfcs[device]
-        #     setpoint = self.mks.read_setpoint(addr)
-        #     self.pullsocket.set_point_now(device + '_setpoint', setpoint)
+        # Consider to also add current measurement type
+        socket_names = ['current_data', 'measurement_running']
+        timeouts = [5, 1e6]
 
+        self.pullsocket = DateDataPullSocket(
+            'NoiseReaderControl', socket_names, timeouts=timeouts, port=9000
+        )
+        self.pullsocket.start()
         self.pushsocket = DataPushSocket('NoiseSpectrumRecorder', action='enqueue')
         self.pushsocket.start()
 
-        # self.livesocket = LiveSocket('furnace_mks_flows', devices)
+        # self.livesocket = LiveSocket('NoiseSpectrumRecorder', socket_names)
         # self.livesocket.start()
         self.running = True
 
+    def _start_measurement(self, element):
+        comment = element.get('comment', '-')
+        print('Start measurement')
+        measurement = element['measurement']
+
+        if measurement == 'record_noise_spectrum':
+            print('Noise Spectrum. Element is: ', element)
+            t = threading.Thread(target=self.nsr.record_spectrum, kwargs=element)
+            t.start()
+
+        elif measurement == 'record_xy_spectrum':
+            print('XY spectrum. Element is: ', element)
+            t = threading.Thread(target=self.nsr.record_xy_measurement, kwargs=element)
+            t.start()
+
+        elif measurement == 'record_sweeped_xy_measurement':
+            print('Sweeped XY. Element is: ', element)
+            t = threading.Thread(
+                target=self.nsr.record_sweeped_xy_measurement, kwargs=element
+            )
+            t.start()
+
     def run(self):
         while self.running:
-            time.sleep(0.1)
+            time.sleep(0.5)
+            # If want a live-socket, set here as well
+            self.pullsocket.set_point_now(
+                'measurement_running', self.nsr.measurement_running
+            )
+            current_data = self.nsr.read_current_data()
+            print('Current data ', current_data)
+            self.pullsocket.set_point_now('current_data', current_data)
+
             qsize = self.pushsocket.queue.qsize()
             while qsize > 0:
                 element = self.pushsocket.queue.get()
-                # cmd = list(element.keys())[0]
                 cmd = element['cmd']
-                if cmd == 'start_measurement':
-                    comment = element.get('comment', '-')
-                    # print('Start measurement')
-                    measurement = element['measurement']
-                    if measurement == 'record_noise_spectrum':
-                        print('Record Noise Spectrum')
-                        t = threading.Thread(
-                            target=self.nsr.record_spectrum,
-                            args=(comment,)
-                        )
-                        t.start()
-
-                    elif measurement == 'record_xy_spectrum':
-                        print('Record XY spectrum')
-                    elif measurement == 'record_sweeped_xy_measurement':
-                        print('Record Sweeped XY measurement')
-
                 if cmd == 'abort':
                     self.nsr.abort_measurement()
-                print('Queue: ' + str(qsize))
-                # self.mks.set_flow(value=element[mfc], addr=self.mfcs[mfc])
-                # self.pullsocket.set_point_now(mfc + '_setpoint', element[mfc])
+                if cmd == 'start_measurement':
+                    self._start_measurement(element)
                 qsize = self.pushsocket.queue.qsize()
-
-            # print('')
-            # for mfc in self.mfcs:
-            #     flow = self.mks.read_flow(self.mfcs[mfc])
-            #     print(mfc + ': ' + str(flow))
-            #     self.pullsocket.set_point_now(mfc, flow)
-            #     # self.livesocket.set_point_now(mfc, flow)
+                print('Queue: ' + str(qsize))
 
 
 def main():
-    """ Main function """
+    """Main function"""
     nsr_control = NoiseSpectrumRecorderControl()
     nsr_control.start()
 
