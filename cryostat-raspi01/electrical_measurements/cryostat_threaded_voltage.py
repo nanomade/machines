bfdcbbb67d4e1f803c1d456921c15ae008fc2834
Robert Jensen <jensen.robert@gmail.com>
2023-10-18 09:10:07 +0200

    Various tests for error detection and correction

--- cryostat-raspi01/electrical_measurements/cryostat_threaded_voltage.py
+++ cryostat-raspi01/electrical_measurements/cryostat_threaded_voltage.py
@@ -20,16 +20,30 @@ class BackgroundMeasure():
         time.sleep(nplc * 0.02 * 0.9)
         self._start_measurement()
 
+
 class MeasureVxx(BackgroundMeasure):
     def __init__(self, current_source):
         super().__init__()
         self.current_source = current_source
 
     def _start_measurement(self):
+        self.current_source._2182a_comm(':STATus:QUEue:NEXT?')
+        v_xx_error_queue = self.current_source._2182a_comm()
+        # TODO! Read this into an network-accesible variable to show in frontend
+        # print('Vxx error queue: ', v_xx_error_queue)
         self.current_source._2182a_comm(':DATA:FRESh?')
         value_raw = self.current_source._2182a_comm()
+
+        # This seems to never trigger - consider to remove
+        if len(value_raw) < 2:
+            print('Try reading again')
+            time.sleep(0.075)
+            value_raw = self.current_source._2182a_comm()
+            print('Second attempt: ', value_raw)
+
         try:
             value_raw = value_raw.replace(chr(0x13), '')
+            # print('Vxx value raw: {}'.format(value_raw))
             voltage = float(value_raw)
         except ValueError:
             voltage = -1001
@@ -42,7 +56,10 @@ class MeasureVxy(BackgroundMeasure):
         self.nano_v = nano_v
 
     def _start_measurement(self):
-        voltage = self.nano_v.read_fresh()
+        try:
+            voltage = self.nano_v.read_fresh()
+        except TypeError:
+            voltage = None
         if voltage is None:
             voltage = -1001
         self.voltage = voltage
