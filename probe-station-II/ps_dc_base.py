58e37c5e9f181bbd12cc77dc9ed7cd1988a8df97
Robert Jensen <jensen.robert@gmail.com>
2023-12-05 15:20:46 +0100

    Allow measurements to work for both I and V source

--- probe-station-II/ps_dc_base.py
+++ probe-station-II/ps_dc_base.py
@@ -21,7 +21,7 @@ class ProbeStationDCBase(ProbeStationMeasurementBase):
         self.dmm.set_integration_time(2)
         self.dmm.scpi_comm(':INIT:CONT ON')  # TODO: Add this to driver
 
-    def _configure_back_gate(self, source_range, current_limit, nplc):
+    def _configure_back_gate(self, source_range, limit, nplc):
         """
         Configure the 2450 for gating.
         """
@@ -29,8 +29,8 @@ class ProbeStationDCBase(ProbeStationMeasurementBase):
         self.back_gate.set_source_function(function='v', source_range=source_range)
         # Temporarily set sense to auto to avoid temporary range conflicts
         self.back_gate.set_sense_function(function='i', sense_range=0)
-        self.back_gate.set_current_limit(current_limit)
-        self.back_gate.set_sense_function(function='i', sense_range=current_limit)
+        self.back_gate.set_current_limit(limit)
+        self.back_gate.set_sense_function(function='i', sense_range=limit)
 
         # Always use 2-point measurement for the gate
         self.back_gate.remote_sense(False)
@@ -44,21 +44,31 @@ class ProbeStationDCBase(ProbeStationMeasurementBase):
         self.back_gate.set_auto_zero('i', True)
         self.back_gate.auto_zero_now()
 
-    def _configure_source(self, source_range, current_limit, nplc, remote_sense=False):
+    def _configure_source(
+        self, function, source_range, limit, nplc, remote_sense=False
+    ):
         """
         Configure souce-drain as a voltage source
         """
         self.source.clear_buffer('iv_data')
-        self.source.set_source_function('v', source_range=source_range)
-        # Temporarily set sense to auto to avoid temporary range conflicts
-        self.source.set_sense_function('i', sense_range=0)
-        self.source.set_current_limit(current_limit)
-        self.source.set_sense_function('i', sense_range=current_limit)
+
+        if function.lower() == 'v':
+            self.source.set_source_function('v', source_range=source_range)
+            # Temporarily set sense to auto to avoid temporary range conflicts
+            self.source.set_sense_function('i', sense_range=0)
+            self.source.set_current_limit(limit)
+            self.source.set_sense_function('i', sense_range=limit)
+            self.source.set_voltage(0)
+        else:
+            self.source.set_source_function('i', source_range=source_range)
+            # Temporarily set sense to auto to avoid temporary range conflicts
+            self.source.set_sense_function('v', sense_range=0)
+            self.source.set_voltage_limit(limit)
+            self.source.set_sense_function('v', sense_range=limit)
+            self.source.set_current(0)
+
         self.source.set_integration_time(nplc)
-
-        self.back_gate.remote_sense(remote_sense)
-
-        self.source.set_voltage(0)
+        self.source.remote_sense(remote_sense)
         self.source.output_state(True)
 
         # TEST WITH AZERO BOTH TRUE AND FALSE!!!!
@@ -84,7 +94,7 @@ class ProbeStationDCBase(ProbeStationMeasurementBase):
         self.add_to_current_measurement(data)
         return reading['source_value']
 
-    def read_source(self, nplc=None, read_dmm=True):
+    def read_source(self, function='v', read_dmm=True):
         if read_dmm:
             v_total = self.dmm.read_dc_voltage()
         else:
@@ -93,11 +103,15 @@ class ProbeStationDCBase(ProbeStationMeasurementBase):
         self.source.trigger_measurement(buffer='iv_data')
         reading = self.source.read_latest(buffer='iv_data')
         # Todo: Check status if device is in compliance
-        data = {
-            # 'v_total': v_total,
-            'v_xx': reading['source_value'],
-            'current': reading['value'],
-        }
+
+        if function == 'v':
+            v_xx = (reading['source_value'],)
+            current = reading['value']
+        else:
+            v_xx = (reading['value'],)
+            current = reading['source_value']
+
+        data = {'v_xx': v_xx, 'current': current}
         if v_total is not None:
             data['v_total'] = v_total
         self.add_to_current_measurement(data)
