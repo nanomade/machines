90cc93b8458afa43711bd342b2b61434431543c4
Robert Jensen <jensen.robert@gmail.com>
2024-03-05 14:41:39 +0100

    Mapping for current software version of system
    RF supply is currently out of order, so lots of values are missing

---
+++ moorfield-3218-raspi01/mapping.py
@@ -0,0 +1,20 @@
+system_name = 'Moorfield 3218'
+data_table = 'moorfield_3218'
+mapping = {
+    'full_range_pressure': 397,
+    'baratron_pressure': 1200,
+    'mfc_1_flow': 1103,
+    'mfc_1_setpoint': None,
+    'mfc_2_flow': 1105,
+    'mfc_2_setpoint': None,
+    'mfc_3_flow': None,
+    'mfc_3_setpoint': None,
+
+    'rf_forward_power': None,
+    'rf_reflected_power': None,
+    'dc_bias': None,
+    'tune_motor': None,
+    'load_motor': None,
+
+    'turbo_speed': None,
+}
