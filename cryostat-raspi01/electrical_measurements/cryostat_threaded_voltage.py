8a53bd5574ec33e14313d76cba31e918905bee93
Robert Jensen <jensen.robert@gmail.com>
2024-08-01 13:41:49 +0200

    Finally get tid of the mess of trying to convince the 6221 to read the 2182a

--- cryostat-raspi01/electrical_measurements/cryostat_threaded_voltage.py
+++ cryostat-raspi01/electrical_measurements/cryostat_threaded_voltage.py
@@ -22,33 +22,18 @@ class BackgroundMeasure():
 
 
 class MeasureVxx(BackgroundMeasure):
-    def __init__(self, current_source):
+    def __init__(self, nano_v):
         super().__init__()
-        self.current_source = current_source
+        self.nano_v = nano_v
 
     def _start_measurement(self):
-        self.current_source._2182a_comm(':STATus:QUEue:NEXT?')
-        v_xx_error_queue = self.current_source._2182a_comm()
-        # TODO! Read this into an network-accesible variable to show in frontend
-        # print('Vxx error queue: ', v_xx_error_queue)
-        self.current_source._2182a_comm(':DATA:FRESh?')
-        value_raw = self.current_source._2182a_comm()
-
-        # This seems to never trigger - consider to remove
-        if len(value_raw) < 2:
-            print('Try reading again')
-            time.sleep(0.075)
-            value_raw = self.current_source._2182a_comm()
-            print('Second attempt: ', value_raw)
-
         try:
-            value_raw = value_raw.replace(chr(0x13), '')
-            # print('Vxx value raw: {}'.format(value_raw))
-            voltage = float(value_raw)
-        except ValueError:
+            voltage = self.nano_v.read_fresh()
+        except TypeError:
+            voltage = None
+        if voltage is None:
             voltage = -1001
         self.voltage = voltage
-
 
 class MeasureVxy(BackgroundMeasure):
     def __init__(self, nano_v):
