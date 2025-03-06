ccec578d5da3802e294d4d7c62a5c8b67cd9ed46
Robert Jensen <jensen.robert@gmail.com>
2025-03-06 14:56:24 +0100

    Working towards a fully working probe-station
    This is an end-of-day-commit, but real functinality is
    approaching fast.

--- probe-station-II/ps_double_stepped_2point_dc_v_source.py
+++ probe-station-II/ps_double_stepped_2point_dc_v_source.py
@@ -1,9 +1,9 @@
 import time
 import numpy as np
-from ps_dc_base import ProbeStationDCBase
+from ps_measurement_base import ProbeStationMeasurementBase
 
 
-class ProbeStation2PointDoubleSteppedVSource(ProbeStationDCBase):
+class ProbeStation2PointDoubleSteppedVSource(ProbeStationMeasurementBase):
     def __init__(self):
         # super().__init__(self=self)
         super().__init__()
@@ -44,44 +44,6 @@ class ProbeStation2PointDoubleSteppedVSource(ProbeStationDCBase):
         )
         self.reset_current_measurement('2PointDoubleSteppedVSource')
 
-    def _configure_instruments(self, source, gate):
-        # Configure instruments:
-        print('Configure Back gate')
-        gate_range = max(abs(gate['v_low']), abs(gate['v_high']))
-        self._configure_back_gate(
-            source_range=gate_range, limit=gate['limit'], nplc=gate['nplc']
-        )
-
-        source_range = max(abs(source['v_high']), abs(source['v_low']))
-        print('Configure Source')
-        self._configure_source(
-            function='v',
-            source_range=source_range,
-            limit=source['limit'],
-            nplc=source['nplc'],
-            remote_sense=False,
-        )
-        print('Configure done')
-
-    def _ramp_gate(self, v_from, v_to, rate=0.5, force_even_if_abort=False):
-        # Rate is the allowed gate-sweep rate in V/s
-        # todo: strongly consider to move this up to dc_base
-        sign = np.sign(v_to - v_from)
-        if sign == 0:
-            # self.back_gate.set_voltage(0)
-            self.back_gate.set_output_level(0)
-            return
-        step_size = 0.025
-
-        if abs(v_to - v_from) < 3 * step_size:
-            # This is not really a ramp, this is a continous sweep that we can
-            # accept to performin one go:
-            msg = 'Small step, set gate directly: {:.1f}mV'
-            print(msg.format(1000 * abs(v_to - v_from)))
-            # self.back_gate.set_voltage(v_to)
-            self.back_gate.set_output_level(v_to)
-            return
-
         print('Ramp gate: ', v_from, v_to, step_size, sign)
         ramp_list = list(np.arange(v_from, v_to, step_size * sign)) + [v_to]
         for gate_ramp_v in ramp_list:
@@ -91,20 +53,21 @@ class ProbeStation2PointDoubleSteppedVSource(ProbeStationDCBase):
                 print('Measurement aborted - stop gate ramp')
                 break
             print('Ramping gate to {}'.format(gate_ramp_v))
-            self.back_gate.set_output_level(gate_ramp_v)
-            self.read_gate()
-            self.read_source(function='v', read_dmm=False)
+            self.tsp_link.set_output_level(gate_ramp_v, node=1)
+            self.read()
             time.sleep(step_size / rate)
 
     def dc_2_point_measurement_v_source(
-        self, comment, inner: str, source: dict, gate: dict, **kwargs
+        self, comment, inner: str, source: dict, gate: dict,
+            params: dict, **kwargs
     ):
         """
         Perform a 2-point DC vi-measurement.
         """
+        # TODO! Store information about readback and auto-zero in metadata!
         self._setup_data_log(comment=comment, source=source, gate=gate)
-        self._configure_instruments(source=source, gate=gate)
-
+        self._configure_instruments(source=source, gate=gate, params=params)
+        
         # Calculate the sweeps
         gate_steps = self._calculate_steps(**gate)
         source_steps = self._calculate_steps(**source)
@@ -113,22 +76,23 @@ class ProbeStation2PointDoubleSteppedVSource(ProbeStationDCBase):
         if inner.lower() == 'source':
             inner_steps = source_steps
             outer_steps = gate_steps
-            inner_inst = self.source
-            outer_inst = self.back_gate
-            # TODO: override set_voltage into ramp, should be implented
-            # at the point where the instruments are assigned (in measurement_base)
+            inner_node = 2  # Source
+            outer_node = 1  # Gate
         else:
             inner_steps = gate_steps
             outer_steps = source_steps
-            inner_inst = self.back_gate
-            outer_inst = self.source
+            inner_node = 1  # Gate
+            outer_node = 2  # Source
 
         latest_inner = 0
         for outer_v in outer_steps:
             if self.current_measurement['type'] == 'aborting':
                 continue
-            outer_inst.set_output_level(outer_v)
+            
             print('Set outer to: {}'.format(outer_v))
+            self.tsp_link.auto_zero_now(node=1)
+            self.tsp_link.auto_zero_now(node=2)
+            self.tsp_link.set_output_level(outer_v, node=outer_node)
 
             for inner_v in inner_steps:
                 if self.current_measurement['type'] == 'aborting':
@@ -136,72 +100,68 @@ class ProbeStation2PointDoubleSteppedVSource(ProbeStationDCBase):
                     # rest of the steps
                     continue
 
+                print('Set inner to {}'.format(inner_v))
                 if inner.lower() == 'source':
-                    inner_inst.set_output_level(inner_v)
+                    self.tsp_link.set_output_level(inner_v, node=inner_node)
                 else:
                     self._ramp_gate(v_from=latest_inner, v_to=inner_v)
                     latest_inner = inner_v
+                time.sleep(params['source_measure_delay'])
 
-                print('Set inner to {}'.format(inner_v))
-                time.sleep(0.05)
                 if not self._check_I_source_status():
                     # TODO!!! This always returns True!!!!
                     return
-
                 # This is a 2-wire measurement, no need for DMM
-                self.read_source(function='v', read_dmm=False)
-                self.read_gate()
+                self.read(read_dmm=False)
 
         time.sleep(2)
 
+        data = self.read()
+        v_from = data['v_backgate']
         if not self.aborted:
-            # Ramp gate back to zero
-            reading = self.back_gate.read_latest()
-            self._ramp_gate(v_from=reading['source_value'], v_to=0)
+            self._ramp_gate(v_from=v_from, v_to=0)
             self.reset_current_measurement(None)
         else:
             print('Ramp gate back to zero')
-            self.back_gate.trigger_measurement()
-            reading = self.back_gate.read_latest()
-            v_from = reading['source_value']
             self._ramp_gate(v_from=v_from, v_to=0, force_even_if_abort=True)
             self.reset_current_measurement(None, error='Aborted')
 
         # Indicate that the measurement is completed
         self.aborted = False
-        self.back_gate.output_state(False)
-        self.source.output_state(False)
+        self.tsp_link.output_state(False, node=1)
+        self.tsp_link.output_state(False, node=2)
         self.reset_current_measurement(None)
 
     def test(self):
-        # self.instrument_id()
-        self.dc_4_point_measurement(
+        self.dc_2_point_measurement_v_source(
             comment='test() - double stepped',
-            inner='source',  # outer will be gate
-            # inner='gate',  # ourter will be source
-            # NPLC?
+            # inner='source',  # outer will be gate
+            inner='gate',  # ourter will be source
+            params = {
+                'autozero': False,
+                'readback': False,
+                'source_measure_delay':  1e-3
+            },
             source={
                 'v_low': -0.2,
-                'v_high': 0.2,
-                'repeats': 2,
-                'steps': 50,
-                'nplc': 5,
-                'limit': 2e-3,
+                'v_high': 1.0,
+                'repeats': 0,
+                'steps': 5,
+                'limit': 1e-2,
                 'nplc': 1,
                 'step_type': 'linear',
             },
             gate={
-                'v_low': -5.0,
+                'v_low': -1.0,
                 'v_high': 5.0,
-                'steps': 3,
-                'repeats': 4,
+                'steps': 101,
+                'repeats': 1,
                 'nplc': 1,
-                'limit': 1e-7,
+                'limit': 1e-5,
                 'step_type': 'linear',
             },
         )
 
-
 if __name__ == '__main__':
-    ps = ProbeStation4PointDC()
+    ps = ProbeStation2PointDoubleSteppedVSource()
     ps.test()
--- probe-station-II/ps_measurement_base.py
+++ probe-station-II/ps_measurement_base.py
@@ -29,12 +29,13 @@ CURRENT_MEASUREMENT_PROTOTYPE = {
 class ProbeStationMeasurementBase(object):
     def __init__(self):
         self.current_measurement = CURRENT_MEASUREMENT_PROTOTYPE.copy()
-        self.back_gate = Keithley2450(interface='lan', hostname='192.168.0.3')
-        self.source = Keithley2450(interface='lan', hostname='192.168.0.4')
+        self.tsp_link =  Keithley2450(interface='lan', hostname='192.168.0.3')
+        self.tsp_link.instr.timeout = 10000
+
         self.dmm = Keithley2100(
             interface='usbtmc', visa_string='USB::0x05E6::0x2100::INSTR'
         )
-
+        
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.setblocking(1)
         self.sock.settimeout(1.0)
@@ -50,35 +51,6 @@ class ProbeStationMeasurementBase(object):
         )
         self.data_set_saver.start()
 
-    # Todo, take arguments to check only the needed instruments, or
-    # check each instrument but report error only if it is not None
-    def _identify_all_instruments(self):
-        found_all = True
-        back_gate_id = self.back_gate.read_software_version()
-        if back_gate_id.find('2450') > 0:
-            # print('Found Keithley 2450 at 192.168.0.3')
-            pass
-        else:
-            print('Did NOT find model 2450 at 192.168.0.3!!!!!!')
-            found_all = False
-
-        source_id = self.source.read_software_version()
-        if source_id.find('2450') > 0:
-            # print('Found Keithley 2450 at 192.168.0.4')
-            pass
-        else:
-            print('Did NOT find model 2450 at 192.168.0.4!!!!!!')
-            found_all = False
-
-        dmm_id = self.dmm.read_software_version()
-        if dmm_id.find('MODEL 2100') > 0:
-            # print('Found Keithley 2000 in USB port')
-            pass
-        else:
-            print('Did NOT find Keithley 2100 in USB port!!!')
-            found_all = False
-        return found_all
-
     def _read_socket(self, cmd):
         try:
             self.sock.sendto(cmd.encode(), ('127.0.0.1', 9000))
@@ -90,6 +62,47 @@ class ProbeStationMeasurementBase(object):
             value = None
         return value
 
+    def prepare_tsp_triggers(self):
+        preparescript = """
+        -- Prepare trigger to accept TSP-triggers
+        tsplink.initialize()
+        trigger.model.load("Empty")
+        node[2].trigger.model.load("Empty")
+        tsplink.line[1].reset()
+        tsplink.line[1].mode = tsplink.MODE_SYNCHRONOUS_MASTER
+        tsplink.line[2].mode = tsplink.MODE_SYNCHRONOUS_ACCEPTOR
+        trigger.tsplinkout[1].stimulus = trigger.EVENT_NOTIFY1
+        trigger.tsplinkin[2].clear()
+        trigger.tsplinkin[2].edge = trigger.EDGE_RISING
+
+        node[2].tsplink.line[2].mode = node[2].tsplink.MODE_SYNCHRONOUS_MASTER
+        node[2].tsplink.line[1].mode = node[2].tsplink.MODE_SYNCHRONOUS_ACCEPTOR
+        node[2].trigger.tsplinkout[2].stimulus = node[2].trigger.EVENT_NOTIFY2
+        node[2].trigger.tsplinkin[1].clear()
+        node[2].trigger.tsplinkin[1].edge = node[2].trigger.EDGE_RISING
+
+        -- Build actual trigger model
+        trigger.model.setblock(1, trigger.BLOCK_NOTIFY, trigger.EVENT_NOTIFY1)
+        trigger.model.setblock(2, trigger.BLOCK_MEASURE_DIGITIZE)
+
+        node[2].trigger.model.setblock(1, node[2].trigger.BLOCK_WAIT, node[2].trigger.EVENT_TSPLINK1)
+        node[2].trigger.model.setblock(2, node[2].trigger.BLOCK_MEASURE_DIGITIZE)
+        """
+        self.tsp_link.load_script('preparescript', preparescript)
+        self.tsp_link.execute_script('preparescript')
+        return
+    
+    def configure_dmm(self, v_limit):
+        """
+        Configure  Model 2000 used for 2-point measurement
+        The unit is set up to measure on the buffered output of
+        the 2450.
+        """
+        self.dmm.configure_measurement_type('volt:dc')
+        self.dmm.set_range(v_limit)
+        self.dmm.set_integration_time(2)
+        self.dmm.scpi_comm(':INIT:CONT ON')  # TODO: Add this to driver
+    
     def reset_current_measurement(
         self, measurement_type, error=False, keep_measuring=False
     ):
@@ -173,6 +186,24 @@ class ProbeStationMeasurementBase(object):
         source_ok = True
         return source_ok
 
+    def _ramp_gate(self, v_from, v_to, rate=0.5, force_even_if_abort=False):
+        # Rate is the allowed gate-sweep rate in V/s
+        # todo: strongly consider to move this up to dc_base
+        sign = np.sign(v_to - v_from)
+        if sign == 0:
+            self.tsp_link.set_output_level(0, node=1)
+            return
+        step_size = 0.025
+
+        if abs(v_to - v_from) < 3 * step_size:
+            # This is not really a ramp, this is a continous sweep that we can
+            # accept to performin one go:
+            msg = 'Small step, set gate directly: {:.1f}mV'
+            print(msg.format(1000 * abs(v_to - v_from)))
+            # self.back_gate.set_voltage(v_to)
+            self.tsp_link.set_output_level(v_to, node=1)
+            return
+
     # This code is also used in the Linkham code
     def _calculate_steps(self, v_low, v_high, steps, repeats=1, **kwargs):
         """
@@ -200,6 +231,114 @@ class ProbeStationMeasurementBase(object):
         ) * repeats
         step_list = up + zigzag + down + [0]
         return step_list
+
+    def _configure_instruments(self, source, gate, params):
+        print('Configure tsp-instruments')
+        self.prepare_tsp_triggers()
+
+        gate_range = max(abs(gate['v_low']), abs(gate['v_high']))
+        if 'v_low' in source:
+            source_range = max(abs(source['v_high']), abs(source['v_low']))
+            source_function = 'v'
+            sense_function = 'i'
+        else:
+            source_range = max(abs(source['i_high']), abs(source['i_low']))
+            source_function = 'i'
+            sense_function = 'v'
+        
+        # TODO!!!! THIS REALLY NEED TO GO INTO ps_measurements_base - the code
+        # is essentially the same for both measurement modes!
+        self.tsp_link.clear_output_queue()
+        self.tsp_link.use_rear_terminals(node=1)
+        self.tsp_link.use_rear_terminals(node=2)
+
+        self.tsp_link.set_source_function(function='v', source_range=gate_range, node=1)
+        # Temporarily set sense to auto to avoid temporary range conflicts
+        self.tsp_link.set_sense_function(function='i', sense_range=0, node=1)
+        
+        self.tsp_link.set_limit(gate['limit'], node=1)
+        self.tsp_link.set_sense_function(function='i', sense_range=gate['limit'], node=1)
+        # Always use 2-point measurement for the gate
+        self.tsp_link.remote_sense(False, node=1)
+        self.tsp_link.set_integration_time(nplc=gate['nplc'], node=1)
+
+        self.tsp_link.set_source_function(source_function, source_range=source_range, node=2)
+        # Temporarily set sense to auto to avoid temporary range conflicts
+        self.tsp_link.set_sense_function(sense_function, sense_range=0, node=2)
+        self.tsp_link.set_limit(source['limit'], node=2)
+        self.tsp_link.set_sense_function(sense_function, sense_range=source['limit'], node=2)
+        self.tsp_link.set_integration_time(nplc=source['nplc'], node=2)
+        
+        for node in (1, 2):
+            time.sleep(0.25)
+            self.tsp_link.clear_buffer(node=node)
+            self.tsp_link.set_readback(action=params['readback'], node=node)
+            self.tsp_link.set_output_level(0, node=node)
+            self.tsp_link.output_state(True, node=node)
+            self.tsp_link.set_auto_zero(params['autozero'], node=node)
+            self.tsp_link.auto_zero_now(node=node) 
+            self.tsp_link.clear_buffer(node=node)
+
+        # If remote sense is activated with output off, a warning is issued
+        if 'v_low' in source:
+            self.tsp_link.remote_sense(False, node=2)
+        else:
+            self.tsp_link.remote_sense(True, node=2)
+
+        # Note: The model of running the trigger model with a single measurement
+        # and repeatedly trigger seems to have an overhead of approximately ~0.3NPLC
+        # compared to a fully configured trigger sweep
+        execute_iteration = """
+        node[2].trigger.model.initiate()
+        trigger.model.initiate()
+        waitcomplete()
+        n = node[1].defbuffer1.endindex
+        m = node[2].defbuffer1.endindex
+        printbuffer(n, n, node[1].defbuffer1, node[1].defbuffer1.units, node[1].defbuffer1.sourcevalues)
+        printbuffer(m, m, node[2].defbuffer1, node[2].defbuffer1.units, node[2].defbuffer1.sourcevalues)
+        print("end " .. n .. " " .. m)
+        """
+        self.tsp_link.load_script('execute_iteration', execute_iteration)
+        print('Configure done')
+    
+    def read(self, read_dmm=False):
+        self.tsp_link.execute_script('execute_iteration')
+        # This script always output exactly three lines
+        gate = self.tsp_link.instr.read().strip().split(',')
+        source = self.tsp_link.instr.read().strip().split(',')
+
+        if 'Amp' in source[1]:
+            current = float(source[0])
+            v_xx =  float(source[2])
+        else:
+            current = float(source[2])
+            v_xx =  float(source[0])
+            
+        
+        # Control should always be 'end n m' with n and m both being the
+        # current iteration number
+        # Todo: Assert this...
+        control = self.tsp_link.instr.read().strip()
+        # print('Control is: ', control)
+        # Todo: Check status if device is in compliance
+        data = {
+            'i_backgate': float(gate[0]),
+            'v_backgate': float(gate[2]),
+            'v_xx': v_xx,
+            'current': current,
+        }      
+
+        # TODO! This needs to be trigger based - or at least software triggered before
+        # the tsp script is executed
+        if read_dmm:
+            v_total = self.dmm.read_dc_voltage()
+        else:
+            v_total = None
+        if v_total is not None:
+            data['v_total'] = v_total
+
+        self.add_to_current_measurement(data)
+        return data
 
     def dummy_background_measurement(self):
         # This should be a simple measurement that runs
