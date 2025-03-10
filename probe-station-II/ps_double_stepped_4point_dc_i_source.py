8a7bb1fafc2a65b033bf1edd5b16172265efad55
Robert Jensen <jensen.robert@gmail.com>
2025-03-10 14:27:32 +0100

    First adaption for contstant-I measurement using TSP-link
    Too much code is copied between the two measurement modes, a
    refactoring is needed.

--- probe-station-II/ps_double_stepped_4point_dc_i_source.py
+++ probe-station-II/ps_double_stepped_4point_dc_i_source.py
@@ -12,7 +12,7 @@ class ProbeStation4PointDoubleSteppedISource(ProbeStationDCBase):
 
     def abort_measurement(self):
         print('ABORT')
-        self.source.set_current(0)
+        self.tsp_link.set_output_level(0)
         self.aborted = True
         self.reset_current_measurement('aborting', error='Aborted', keep_measuring=True)
 
@@ -33,49 +33,9 @@ class ProbeStation4PointDoubleSteppedISource(ProbeStationDCBase):
         )
         self.reset_current_measurement('4PointDoubleStepped')
 
-    def _configure_instruments(self, source, gate):
-        # Configure instruments:
-        print('Configure Back gate')
-        gate_range = max(abs(gate['v_low']), abs(gate['v_high']))
-        self._configure_back_gate(
-            source_range=gate_range, limit=gate['limit'], nplc=source['nplc']
-        )
-
-        source_range = max(abs(source['v_low']), abs(source['v_high']))
-        print('Configure DMM')
-        self._configure_dmm(v_limit=source_range)
-
-        print('Configure Source')
-        self._configure_source(
-            function='i',
-            source_range=source_range,
-            limit=source['limit'],
-            nplc=source['nplc'],
-            remote_sense=True,
-        )
-        print('Configure done')
-
-    def _ramp_gate(self, v_from, v_to, rate=0.5, force_even_if_abort=False):
-        # Rate is the allowed gate-sweep rate in V/s
-        # todo: strongly consider to move this up to dc_base
-        sign = np.sign(v_to - v_from)
-        step_size = 0.025
-
-        ramp_list = list(np.arange(v_from, v_to, step_size * sign)) + [v_to]
-        for gate_ramp_v in ramp_list:
-            if (self.current_measurement['type'] == 'aborting') and (
-                not force_even_if_abort
-            ):
-                print('Measurement aborted - stop gate ramp')
-                break
-            print('Ramping gate to {}'.format(gate_ramp_v))
-            self.back_gate.set_output_level(gate_ramp_v)
-            self.read_gate()
-            self.read_source(function='i')
-            time.sleep(step_size / rate)
-
     def dc_4_point_measurement_i_source(
-        self, comment, inner: str, source: dict, gate: dict, **kwargs
+            self, comment, inner: str, source: dict, gate: dict,
+            params: dict, **kwargs
     ):
         """
         Perform a 4-point DC vi-measurement.
@@ -87,92 +47,110 @@ class ProbeStation4PointDoubleSteppedISource(ProbeStationDCBase):
         :v_limit: Maximal allowed voltage, default is 1.0
         """
         self._setup_data_log(comment=comment, source=source, gate=gate)
-        self._configure_instruments(source=source, gate=gate)
+        self._configure_instruments(source=source, gate=gate, params=params)
 
         # Calculate the sweeps
-        gate_steps = self._calculate_steps(**gate)
-        source_steps = self._calculate_steps(**source)
+        gate_steps = self._calculate_steps(
+            low=gate['v_low'],
+            high=gate['v_high'],
+            repeats=gate['repeats'],
+            steps=gate['steps'],
+        )
+        source_steps = self._calculate_steps(
+            low=source['i_low'],
+            high=source['i_high'],
+            repeats=source['repeats'],
+            steps=source['steps'],
+        )
 
         assert inner.lower() in ('source', 'gate')
         if inner.lower() == 'source':
             inner_steps = source_steps
             outer_steps = gate_steps
-            inner_inst = self.source
-            outer_inst = self.back_gate
-            # TODO: override set_voltage into ramp, should be implented
-            # at the point where the instruments are assigned (in measurement_base)
+            inner_node = 2  # Source
+            outer_node = 1  # Gate
         else:
             inner_steps = gate_steps
             outer_steps = source_steps
-            inner_inst = self.back_gate
-            outer_inst = self.source
+            inner_node = 1  # Gate
+            outer_node = 2  # Source
 
-        print('Inner steps are: ', inner_steps)
-        print('Outer steps are: ', outer_steps)
-
-        # Ramp to the first gate voltage:
-        self._ramp_gate(v_from=0, v_to=gate['v_low'])
-
+        latest_inner = 0
         for outer_v in outer_steps:
             if self.current_measurement['type'] == 'aborting':
                 continue
-            outer_inst.set_output_level(outer_v)
-            print('Set outer to: {}'.format(outer_v))
 
-            for inner_i in inner_steps:
+            print('Set outer to: {}'.format(outer_v))
+            self.tsp_link.auto_zero_now(node=1)
+            self.tsp_link.auto_zero_now(node=2)
+            self.tsp_link.set_output_level(outer_v, node=outer_node)
+
+            for inner_v in inner_steps:
                 if self.current_measurement['type'] == 'aborting':
                     # Measurement has been aborted, skip through the
                     # rest of the steps
                     continue
-                inner_inst.set_output_level(inner_i)
-                print('Set inner to {}'.format(inner_i))
-                time.sleep(0.05)
+
+                print('Set inner to {}'.format(inner_v))
+                if inner.lower() == 'source':
+                    self.tsp_link.set_output_level(inner_v, node=inner_node)
+                else:
+                    self._ramp_gate(v_from=latest_inner, v_to=inner_v)
+                    latest_inner = inner_v
+                time.sleep(params['source_measure_delay'])
+
                 if not self._check_I_source_status():
                     # TODO!!! This always returns True!!!!
                     return
-
-                self.read_source(function='i')  # Also reads 2-point via the DMM
-                self.read_gate()
+                # Todo: This is a 4 (3....) point measurement - read DMM!
+                self.read(read_dmm=False)
 
         time.sleep(2)
 
+        data = self.read()
+        v_from = data['v_backgate']
         if not self.aborted:
-            # Ramp gate back to zero
-            self._ramp_gate(v_from=gate['v_high'], v_to=0)
+            self._ramp_gate(v_from=v_from, v_to=0)
             self.reset_current_measurement(None)
         else:
             print('Ramp gate back to zero')
-            self.back_gate.trigger_measurement()
-            reading = self.back_gate.read_latest()
-            v_from = reading['source_value']
             self._ramp_gate(v_from=v_from, v_to=0, force_even_if_abort=True)
             self.reset_current_measurement(None, error='Aborted')
 
         # Indicate that the measurement is completed
         self.aborted = False
-        self.back_gate.output_state(False)
-        self.source.output_state(False)
+        self.tsp_link.output_state(False, node=1)
+        self.tsp_link.output_state(False, node=2)
         self.reset_current_measurement(None)
 
     def test(self):
         # self.instrument_id()
         self.dc_4_point_measurement_i_source(
             comment='test() - double stepped',
-            inner='source',  # outer will be gate
-            # inner='gate',  # ourter will be source
+            # inner='source',  # outer will be gate
+            inner='gate',  # ourter will be source
+            params = {
+                'autozero': False,
+                'readback': False,
+                'source_measure_delay':  1e-3
+            },
             source={
-                'start': -2e-4,
-                'stop': 2e-3,
-                'steps': 50,
-                'limit': 10,
+                'i_low': -5e-4,
+                'i_high': 2e-3,
+                'repeats': 0,
+                'steps': 5,
+                'limit': 2,
                 'nplc': 1,
+                'step_type': 'linear',
             },
             gate={
-                'start': -5.0,
-                'stop': 5.0,
-                'steps': 3,
-                'limit': 1e-7,
+                'v_low': -1.0,
+                'v_high': 5.0,
+                'steps': 101,
+                'repeats': 1,
                 'nplc': 1,
+                'limit': 1e-5,
+                'step_type': 'linear',
             },
         )
 
